c0c00108dd2c0cf25ff3bfe545855c48ba3bd519
TheophileGreg <theophile.gregoire@icloud.com>
2020-09-16 17:22:31 +0200

    create function SetListeners see in Lesson2

--- app/src/main/java/com/example/henripotier_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/henripotier_kotlin/MainActivity.kt
@@ -7,40 +7,42 @@ import android.widget.Button
 import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-import com.example.henripotier_kotlin.databinding.ActivityBinding
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
 
     val cart = mutableSetOf<Book>()
     var book1 = Book("book1Henri", "J.K", 25.0);
     var book2 = Book("book2Henri", "J.K", 35.0);
     var book3 = Book("book3Henri", "J.K", 30.0);
-
+    var book4 = Book("book4Henri", "J.K", 31.0);
+    var book5 = Book("book5Henri", "J.K", 33.0);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
 
 
-        //Redondance set listener sur tout les boutons de la page et passer en parametre de la function pour ajouter le bon livre
-        findViewById<Button>(R.id.book1_button).setOnClickListener{
-            addBook1(it)
+    }
 
-        }
-        findViewById<Button>(R.id.book2_button).setOnClickListener{
-            addBook2(it)
-
-        }
-        findViewById<Button>(R.id.book3_button).setOnClickListener{
-            addBook3(it)
-
+    private fun setListeners(){
+        val clickableView: List<View> = listOf(book1_button, book2_button, book4_button, book3_button, book5_button)
+        for (item in clickableView){
+            item.setOnClickListener{addBook(it)}
         }
     }
 
-    //Comment récupérer le bon livre , if/When ?
-    private fun addBook(view: View, bookAdded: Book) {
-        cart.add(bookAdded)
+    fun addBook(view: View){
+        when(view.id){
+            R.id.book1_button -> cart.add(book1)
+            R.id.book2_button -> cart.add(book2)
+            R.id.book3_button -> cart.add(book3)
+            R.id.book4_button -> cart.add(book4)
+            R.id.book5_button -> cart.add(book5)
+        }
         refreshSum(view)
+        refreshCartDetails(view)
     }
 
     private fun addBook1(view: View) {
