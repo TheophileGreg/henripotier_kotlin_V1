8b175636a4649e5984e08a05fedf0cf357af6a9f
TheophileGreg <theophile.gregoire@icloud.com>
2020-09-25 14:40:41 +0200

    ajout activity

---
+++ app/src/main/java/com/example/henripotier_kotlin/BookDetailActivity.kt
@@ -0,0 +1,35 @@
+package com.example.henripotier_kotlin
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class BookDetailActivity : AppCompatActivity() {
+
+    companion object {
+
+        val EXTRA_BOOK = "BookDetailActivity:book"
+        fun newIntent(context: Context, book: Book): Intent {
+            val detailIntent = Intent(context, BookDetailActivity::class.java)
+
+            detailIntent.putExtra(EXTRA_BOOK, book)
+
+            return detailIntent
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+        val book : Book = intent.getParcelableExtra<Book>(EXTRA_BOOK) ?: return
+
+
+        val titleText = findViewById<TextView>(R.id.titleBook_text);
+
+        titleText.text = " Vous voulez obtenir des détails sur le livre : " + book.cover
+
+    }
+}
